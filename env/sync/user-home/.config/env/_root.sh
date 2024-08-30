aa45f97f966cb5ff511161c65ebdaf94d4b522ab
cmt <no-email@cmt.com>
2024-08-30 08:12:04 +0200

    Synced env to REMOTE

--- env/sync/user-home/.config/env/_root.sh
+++ env/sync/user-home/.config/env/_root.sh
@@ -2,14 +2,18 @@
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-source $SCRIPT_DIR/git.sh
-source $SCRIPT_DIR/k8s.sh
-source $SCRIPT_DIR/nvim.sh
-source $SCRIPT_DIR/bat.sh
-source $SCRIPT_DIR/zoxide.sh
-source $SCRIPT_DIR/starship.sh
-source $SCRIPT_DIR/lsd.sh
-source $SCRIPT_DIR/zypper.sh
+for entry in "$SCRIPT_DIR"/*
+do
+    if [[ ${entry} = *"_root"* ]];then
+        continue
+    fi
+
+    if [[ ${entry} = *"sdkman"* ]];then
+        continue
+    fi
+
+    source "$entry"
+done
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 source $SCRIPT_DIR/sdkman.sh
